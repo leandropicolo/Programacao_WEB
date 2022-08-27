3163211c8ec1d8527d92a5b63d8a43974f883ab8
leandropicolo <99516090+leandropicolo@users.noreply.github.com>
2022-08-27 12:49:57 -0300

    atualizado

---
+++ consumo_combustivel/app/Http/Controllers/CombustivelController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CombustivelController extends Controller
+{
+    //
+
+    public function index(){
+        return view('consumo');
+    }
+
+    public function gasto(){
+        
+    }
+}

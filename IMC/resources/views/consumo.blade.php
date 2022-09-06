a3e297882de7b729f248479d3c726628cadd6819
leandropicolo <99516090+leandropicolo@users.noreply.github.com>
2022-09-06 00:07:19 -0300

    update

--- IMC/resources/views/consumo.blade.php
+++ IMC/resources/views/consumo.blade.php
@@ -3,34 +3,30 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Calculo de Consumo de Combustível</title>
+    <title>Calculo do IMC de uma pessoa</title>
     <link rel="stylesheet" href="{{ asset('css/style.css') }}" type="text/css">
 </head>
 <body>
     <main>
         <div class="painel">
-            <h2>Instruções</h2>
+            <h2>Calculo IMC</h2>
             <div class="conteudo-painel">
-                <p>Esta aplicação tem como finalidade demonstrar os valores que 
-                    serão gastos com combustível durante uma viagem, com base no
-                    consumo do seu veículo, e com a distância determinada por você!
-                </p>
-                <p>Os combustíveis disponíveis para este cálculo são:</p>
-                <ul>
-                    <li><b>Gasolina - 4,80</b></li>
-                </ul>
+                <p>IMC é a sigla para Índice de Massa Corpórea, parâmetro adotado pela Organização Mundial de Saúde para calcular o peso ideal de cada pessoa.</p>
+                <p>O índice é calculado da seguinte maneira: divide-se o peso do paciente pela sua altura elevada ao quadrado. Diz-se que o indivíduo tem peso normal quando o resultado do IMC está entre 18,5 e 24,9.</p>
+                <p>Quer descobrir seu IMC? Insira seu peso e sua altura nos campos abaixo e compare com os índices da tabela. Importante: siga os exemplos e use pontos como separadores.</p>
+                
             </div>
         </div>
 
         <div class="painel">
-            <h2>Cálculo do valor (R$) do consumo</h2>
+            <h2>Cálculo do Índice</h2>
             <div class="conteudo-painel">
                 <form action="{{url('/gasto')}}" method="get">
 
-                    <label for="peso">Distância em Quilômetros a ser percorrida</label>
+                    <label for="peso">Peso da pessoa:</label>
                     <input type="text" class="campoTexto"  name="peso" required/>
 
-                    <label for="altura">Consumo de combustível do veículo (Km/L)</label>
+                    <label for="altura">Altura da Pessoa:</label>
                     <input type="text" class="campoTexto" name="altura" required/>
 
                     <button class="botao" type="submit">Calcular</button>
--- IMC/resources/views/resultado.blade.php
+++ IMC/resources/views/resultado.blade.php
@@ -3,17 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Calculo de Consumo de Combustível</title>
+    <title>Calculo do IMC de uma pessoa</title>
     <link rel="stylesheet" href="{{ asset('css/style.css') }}" type="text/css">
 </head>
 <body>
     <div class="painel">
-        <h2>Resultado do cálculo de consumo</h2>
+        <h2>Resultado do IMC</h2>
         <div class="conteudo-painel">
             <div class='sucesso'>
-                <h3>O valor total gasto será de</h3>
                 <ul>
-                    <li><b>Gasolina:</b> R$ {{$valor}}</li>
+                    <li><b>IMC:</b> R$ {{$valor}}</li>
                 </ul>
             </div>
             <a class="botao" href="{{URL('/')}}" title="voltar">Voltar</a>
